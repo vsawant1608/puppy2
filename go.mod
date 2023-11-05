2e876dcedd355bc51ff2730758d2b92673f69c75
Vishakha Sawant <Vishakha.Sawant2@ibm.com>
2023-11-05 13:16:49 +0530

    fix: code changes

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/vsawant1608/puppy2
 
 go 1.21.1
+
+require github.com/vsawant1608/dog v0.0.0-20231105073921-1a5106365cc3
--- puppy.go
+++ puppy.go
@@ -1,9 +1,18 @@
 package puppy
 
+import (
+	"github.com/vsawant1608/dog"
+	"github.com/vsawant1608/dog/cat"
+)
+
 func Bark() string {
 	return "Woof!"
 }
 
 func Barks() string {
 	return "woof! woof! woof!"
+}
+
+func MixedFunction() string {
+	return "hj" + dog.WhenGrown("Hola") + cat.Fights()
 }
